a182b11547035d4042fecffa6ed6e07a42c98b54
toki.sun <toki.sun.-nd@disney.com>
2023-09-05 13:52:52 +0800

    🍱 add static placeholder

---
+++ docs/_next/static/chunks/pages/_app-ce94d7a7fcdff1bb.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{7839:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return u(4459)}])}},function(n){var _=function(_){return n(n.s=_)};n.O(0,[179],function(){return _(7839),_(6204)}),_N_E=n.O()}]);

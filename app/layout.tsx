80998ffc4fc998cbffb62252e4489dc5b99a1c70
Yagokoro <yagokoro@do-core.cn>
2023-09-09 23:11:39 +0800

    📈 add readme, index and analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css';
 import './tailwind.css';
 
+import Script from 'next/script';
+
 export const metadata = {
   title: '车万皇帝纪行',
   description: '东方, 车万, 车万皇帝, 东方Project, 囧仙, 囧仙子, 哆叽, 魅知',
@@ -9,6 +11,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="zh">
+      <Script
+        strategy="beforeInteractive"
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-NKLTP9H6LC"></Script>
+      <Script
+        strategy="beforeInteractive"
+        id="google-analytics">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-NKLTP9H6LC');
+            `}
+      </Script>
       <body>
         <nav className="flex items-center justify-between flex-wrap bg-neutral-500 p-6">
           <div className="flex items-center flex-shrink-0 text-white mr-6">
--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,66 @@
 'use client';
+import { useState, useCallback } from 'react';
 import { MDXProvider } from '@mdx-js/react';
+import { debounce } from 'lodash';
 
 import PageMDX from './page.mdx';
 
+interface IndexItem {
+  hash: string;
+  text: string;
+}
+
+const _indexs: IndexItem[] = [];
+
 export default function Page() {
+  const [indexs, _setIndexs] = useState<IndexItem[]>([]);
+
+  const setIndexs = useCallback(
+    debounce(() => {
+      _setIndexs(_indexs);
+    }, 500),
+    []
+  );
+
   return (
-    <MDXProvider
-      components={{
-        img: (props) => (
-          <img
-            className="mx-auto my-0"
-            loading="lazy"
-            {...props}
-          />
-        ),
-        h2: (props) => (
-          <h2
-            id={props.children as string}
-            {...props}></h2>
-        ),
-      }}>
-      <PageMDX />
-    </MDXProvider>
+    <>
+      <details>
+        <summary>目录</summary>
+        <ul>
+          {indexs.map(({ hash, text }) => (
+            <li key={hash}>
+              <a href={`#${hash}`}>{text}</a>
+            </li>
+          ))}
+        </ul>
+      </details>
+
+      <MDXProvider
+        components={{
+          img: (props) => (
+            <img
+              className="mx-auto my-0"
+              loading="lazy"
+              {...props}
+            />
+          ),
+          h2: (props) => {
+            _indexs.push({
+              hash: encodeURIComponent(props.children as string),
+              text: props.children as string,
+            });
+
+            setIndexs();
+
+            return (
+              <h2
+                id={encodeURIComponent(props.children as string)}
+                {...props}></h2>
+            );
+          },
+        }}>
+        <PageMDX />
+      </MDXProvider>
+    </>
   );
 }

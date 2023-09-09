4accd55150343dc1fa3931c2bc5c697475f2a872
Yagokoro <yagokoro@do-core.cn>
2023-09-09 18:08:10 +0800

    🎨 优化阅读体验

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,26 @@
-"use client";
-import PageMDX from "./page.mdx";
+'use client';
+import { MDXProvider } from '@mdx-js/react';
+
+import PageMDX from './page.mdx';
 
 export default function Page() {
-  return <PageMDX />;
+  return (
+    <MDXProvider
+      components={{
+        img: (props) => (
+          <img
+            className="mx-auto my-0"
+            loading="lazy"
+            {...props}
+          />
+        ),
+        h2: (props) => (
+          <h2
+            id={props.children as string}
+            {...props}></h2>
+        ),
+      }}>
+      <PageMDX />
+    </MDXProvider>
+  );
 }
--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
-const withMDX = require("@next/mdx")();
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    // If you use remark-gfm, you'll need to use next.config.mjs
+    // as the package is ESM only
+    // https://github.com/remarkjs/remark-gfm#install
+    remarkPlugins: [],
+    rehypePlugins: [],
+    providerImportSource: '@mdx-js/react',
+  },
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  output: 'export',
   cleanDistDir: true,
   /**
    * Disable server-based image optimization. Next.js does not support

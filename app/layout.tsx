a0ac499a939570c069ad02c37708476272fdf675
Yagokoro <yagokoro@do-core.cn>
2023-09-10 16:01:15 +0800

    📈 add SEO and 秘活欠款

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,12 +5,14 @@ import Script from 'next/script';
 
 export const metadata = {
   title: '车万皇帝纪行',
-  description: '东方, 车万, 车万皇帝, 东方Project, 囧仙, 囧仙子, 哆叽, 魅知',
+  description:
+    '东方爱好者不需要车万皇帝。谢谢每一位为东方奉献自己人，但请不要献祭别人。',
+  keywords: '囧仙,赵旭升,车万皇帝,囧仙子,东方,车万,东方Project,哆叽,魅知',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="zh">
+    <html lang="zh-CN">
       <Script
         strategy="beforeInteractive"
         async
@@ -26,12 +28,22 @@ export default function RootLayout({ children }) {
             gtag('config', 'G-NKLTP9H6LC');
             `}
       </Script>
+
       <body>
         <nav className="flex items-center justify-between flex-wrap bg-neutral-500 p-6">
           <div className="flex items-center flex-shrink-0 text-white mr-6">
             <span className="font-semibold text-xl tracking-tight">
               车万皇帝纪行
             </span>
+          </div>
+
+          <div>
+            <a
+              className="text-white"
+              target="_blank"
+              href="https://github.com/touhou-emperor/touhou-emperor.github.io">
+              来Github添砖加瓦
+            </a>
           </div>
         </nav>
 
